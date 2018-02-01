b981a7566b55cb7cb8e1bb13b9bc26e794d744b9
WiseWill <rafael.willian.engcomp@gmail.com>
2018-02-01 20:19:06 -0200

    Criação da struct a ser retornada como o inteiro e função que randomiza um int

--- src/server.go
+++ src/server.go
@@ -4,7 +4,20 @@ import (
 	"log"
 	"github.com/gorilla/mux"
 	"net/http"
+	"encoding/json"
+	"math/rand"
 )
+
+type ApiInt struct {
+	Data int `json:"result"`
+}
+
+//Função que retorna um numero inteiro randomico
+func randomizeInt(generator *rand.Rand, max int) *ApiInt {
+	var retorno = new (ApiInt)
+	retorno.Data = generator.Intn(max)
+	return retorno
+}
 
 func getValue(w http.ResponseWriter, r *http.Request) {
 
