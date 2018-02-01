e00860e7d8246c2f0e1512100192fdc1f690f722
WiseWill <rafael.willian.engcomp@gmail.com>
2018-02-01 21:09:17 -0200

    Adição de comentarios

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -1,5 +1,9 @@
 package main
 
+/*  Autor: Rafael Willian                                                                *
+ *  Descrição: O seguinte código trata-se de um Webserver que retorna, em 90% das        *  
+ * 	chamadas, um número inteiro randômico, e nos outros 10% uma string também randômica  */
+
 import (
 	"log"
 	"time"
@@ -16,10 +20,12 @@ const letters = "abcdefghijklmnopqrstuvwxyz" //Letras para geracao da string ran
 var decadeCounter int //Conta quantas vezes o endpoint foi chamado de 10 em 10
 var stringPosition int //Guarda a posicao que a string deve ser mostrada na decada
 
+//Estrutura de dados que guarda uma string, para padronizar o retorno
 type ApiString struct {
 	Data string `json:"result"`
 }
 
+//Estrutura de dados que guarda um numero inteiro, para padronizar o retorno
 type ApiInt struct {
 	Data int `json:"result"`
 }
@@ -29,6 +35,7 @@ func randomizeString(generator *rand.Rand, size int) *ApiString {
 	var retorno = new (ApiString)
 	str := make([]byte, size)
 
+	//A cada iteração, uma letra da string é selecionada de 'letters'
 	for i := range str {
 		str[i] = letters[generator.Intn(len(letters))]
 	}
@@ -44,6 +51,7 @@ func randomizeInt(generator *rand.Rand, max int) *ApiInt {
 	return retorno
 }
 
+//Função que reinicia a contagem de 10 ou incrementa
 func checkDecade(generator *rand.Rand) {
 	if decadeCounter == decadeMax {
 		decadeCounter = 0
@@ -53,31 +61,35 @@ func checkDecade(generator *rand.Rand) {
 	}
 }
 
+//Função que retorna o valor randomico de acordo com a contagem
 func getValue(w http.ResponseWriter, r *http.Request) {
 	source := rand.NewSource(time.Now().UnixNano())
 	generator := rand.New(source)
 
+	//Caso a contagem coincida com a posição da string uma string é retornada
 	if decadeCounter == stringPosition {
 		json.NewEncoder(w).Encode(randomizeString(generator, 14))
-	} else {
+	} else { //Caso contrário um int é retornado
 		json.NewEncoder(w).Encode(randomizeInt(generator, 100))
 	}
 
 	checkDecade(generator)
 }
 
+//Função que reinicia os dados básicos do Webserver
 func resetServer() {
 	decadeCounter = 0
 	stringPosition = 0
 }
 
 func main() {
-	source := rand.NewSource(time.Now().UnixNano())
-	generator := rand.New(source)
-	stringPosition = generator.Intn(decadeMax)
+	source := rand.NewSource(time.Now().UnixNano()) //Cria uma seed para geração randomica
+	generator := rand.New(source) //Cria um gerador randômico através da seed
+	stringPosition = generator.Intn(decadeMax) //Inicia a posição da string, para a primeira chamada
 
-	router := mux.NewRouter()
-	router.HandleFunc("/reset", getValue).Methods("GET")
+	router := mux.NewRouter() //Cria um roteador de requisições
+	//Configura-se dois endpoints /reset e /value para serem acessados via GET
+	router.HandleFunc("/reset", getValue).Methods("GET") 
 	router.HandleFunc("/value", getValue).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", router)) //Inicia o servidor
 }
